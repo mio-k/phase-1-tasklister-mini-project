284550be9a6ebe82b497d3e065ddcd5f9c2f0bf0
mio kataoka <mio.kanna@gmail.com>
2022-03-13 14:33:27 -0700

    completed the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target[0].value)
+    document.querySelector('form').reset()
+  })
+})
+function handleToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  li.textContent = `${todo} `;
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
